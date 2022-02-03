b882c64f3042d30fc74bbbee7402890982278d51
avinash <avinashsinghytech@gmail.com>
2022-02-03 09:59:39 +0530

    my 1st commit

---
+++ myDemo/myDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myDemo
+//
+//  Created by AVINASH on 03/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
